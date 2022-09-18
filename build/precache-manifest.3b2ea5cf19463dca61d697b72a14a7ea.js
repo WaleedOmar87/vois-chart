3bf3e0addb91f5da825769874bc9bab452346579
WaleedOmar87 <waleedomar87@gmail.com>
2022-09-18 04:53:47 +0200

    build

---
+++ build/precache-manifest.3b2ea5cf19463dca61d697b72a14a7ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0266b0da420e6594ff2c",
+    "url": "/static/css/main.fe76c7c2.chunk.css"
+  },
+  {
+    "revision": "0266b0da420e6594ff2c",
+    "url": "/static/js/main.0266b0da.chunk.js"
+  },
+  {
+    "revision": "81408b52ac98b9e352b8",
+    "url": "/static/js/1.81408b52.chunk.js"
+  },
+  {
+    "revision": "83cf3311100782cccb32",
+    "url": "/static/js/2.83cf3311.chunk.js"
+  },
+  {
+    "revision": "2f8f964cc5d547f7d92b",
+    "url": "/static/js/runtime~main.2f8f964c.js"
+  },
+  {
+    "revision": "4169bfe2484ae255d5afaece0e88552d",
+    "url": "/index.html"
+  }
+];
