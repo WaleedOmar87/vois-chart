0a42e4f9bfabece619d28510d8612be12fb8f9c7
WaleedOmar87 <waleedomar87@gmail.com>
2022-09-20 05:10:43 +0200

    updated dependencies

--- src/test/LineChart.test.tsx
+++ src/test/LineChart.test.tsx
@@ -3,18 +3,9 @@ import { render, act, screen, cleanup, waitFor } from "@testing-library/react";
 import { LineChart } from "./../components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AppProvider } from "../store";
-import "jest-canvas-mock";
 import { default as mockFetch } from "./../mocks/mockFetch";
 import testData from "./../data/testData";
 import "mutationobserver-shim";
-
-// Mock line chart
-class ResizeObserver {
-	observe() {}
-	unobserve() {}
-	disconnect() {}
-}
-window.ResizeObserver = ResizeObserver;
 
 describe("Testing Line Chart", () => {
 	afterEach(() => {
@@ -43,27 +34,4 @@ describe("Testing Line Chart", () => {
 		const getChartContainer = screen.getByTestId("line-chart-container");
 		expect(getChartContainer).toBeInTheDocument();
 	});
-
-	// Testing Loading Line Chart
-	// test("Successfully render chart", async () => {
-	// 	render(
-	// 		<Router>
-	// 			<Routes>
-	// 				<Route
-	// 					path="/"
-	// 					element={
-	// 						<AppProvider
-	// 							value={{
-	// 								loading: false,
-	// 							}}
-	// 						>
-	// 							<LineChart />
-	// 						</AppProvider>
-	// 					}
-	// 				/>
-	// 			</Routes>
-	// 		</Router>
-	// 	);
-	// 	expect(await screen.findByTestId("line-chart-container")).toBeTruthy();
-	// });
 });
